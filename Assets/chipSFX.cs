c34653cefe0322324d2ddda9ce20049a3c0dc548
guillemlloret <guillem.lloret.macia@estudiantat.upc.edu>
2025-01-07 19:05:02 +0100

    sonidos

---
+++ Assets/chipSFX.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class chipSFX : MonoBehaviour
+{
+    public AudioSource soundSource; 
+
+    private void Start()
+    {
+        
+        if (soundSource == null)
+        {
+            soundSource = GetComponent<AudioSource>();
+        }
+
+       
+        if (soundSource == null)
+        {
+            Debug.LogError("No s'ha trobat cap AudioSource assignat o al component.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            
+            if (!soundSource.isPlaying)
+            {
+                soundSource.Play();
+                Debug.Log("Jugador dins del trigger, so activat.");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            
+            soundSource.Stop();
+            Debug.Log("Jugador fora del trigger, so desactivat.");
+        }
+    }
+}
